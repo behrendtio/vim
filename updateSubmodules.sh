bc4acd86a3d7cbef28a0772210dcc39bc4fdd1ee
Mario Behrendt <m.behrendt@yanted.com>
2013-03-08 20:21:32 +0100

    Update script clean up

--- updateSubmodules.sh
+++ updateSubmodules.sh
@@ -13,34 +13,34 @@ LOGFILE=update.log
 DATE=`date`
 
 clear
-echo "Checking submodules"
+echo "Updating submodules"
 
 echo $DATE > $BASEDIR/$LOGFILE
 echo "" >> $BASEDIR/$LOGFILE
 
 for SUBMODULE in `ls bundle`; do
-    cd bundle/$SUBMODULE
+  cd bundle/$SUBMODULE
 
-	echo "Submodule $SUBMODULE" >> $BASEDIR/$LOGFILE
-	echo "==============================" >> $BASEDIR/$LOGFILE
+  echo "Submodule $SUBMODULE" >> $BASEDIR/$LOGFILE
+  echo "==============================" >> $BASEDIR/$LOGFILE
 
-    git checkout master >> $BASEDIR/$LOGFILE 2>&1
-    git pull >> $BASEDIR/$LOGFILE 2>&1
-	echo "" >> $BASEDIR/$LOGFILE
+  git checkout master >> $BASEDIR/$LOGFILE 2>&1
+  git pull >> $BASEDIR/$LOGFILE 2>&1
+  echo "" >> $BASEDIR/$LOGFILE
 
-    cd $BASEDIR
-	echo -n "."
+  cd $BASEDIR
+  echo ".\c"
 done
 
-echo " Done"
 echo ""
+echo "Done"
 echo "Remove update log in $BASEDIR/$LOGFILE? [Y/n]"
 
 read -n 1 -s ANSWER
 
 if  ([ "" == "$ANSWER" ] || [ "Y" == "$ANSWER" ] || [ "y" == "$ANSWER" ]); then
-	rm -f $BASEDIR/$LOGFILE
-	echo "Log deleted"
+  rm -f $BASEDIR/$LOGFILE
+  echo "Log deleted"
 fi
 
 exit 0
